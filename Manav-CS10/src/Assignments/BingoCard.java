9362b502bef45ddbdb272673063e7a407351afad
1005327001 <1005327001@L815WD220024.edu.cbe.ab.ca>
2022-04-11 13:31:30 -0600

    This is my bingo card

---
+++ Manav-CS10/src/Assignments/BingoCard.java
@@ -0,0 +1,28 @@
+/*
+
+Program: MyInitials.java          Date: April 6, 2022
+
+
+Author: Manav Pandya 
+School: CHHS
+Course: Computer Science 10
+ 
+*/
+package Assignments;
+
+public class BingoCard 
+{
+
+	public static void main(String[] args) 
+	{ System.out.println("B\tI\tN\tG\tO");
+	  System.out.println("2\t20\t42\t60\t64");
+	  System.out.println("14\t25\t32\t55\t70");
+	  System.out.println("5\t18\tFREE\t53\t67");
+	  System.out.println("12\t16\t31\t46\t75");
+	  System.out.println("10\t22\t39\t59\t71");
+	}
+
+}
+/* Screen Dump
+ 
+ */
