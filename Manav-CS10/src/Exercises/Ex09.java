57ee1fe1d58ef83621d039c1aa8553d5715fb66d
1005327001 <1005327001@L815WD220024.edu.cbe.ab.ca>
2022-04-12 14:44:26 -0600

    this is ex09

---
+++ Manav-CS10/src/Exercises/Ex09.java
@@ -0,0 +1,13 @@
+package Exercises;
+
+public class Ex09 
+{
+
+	public static void main(String[] args) 
+	{ 
+		double myDouble = 9.78d;
+		int myInt = (int) myDouble;
+		System.out.println(myInt);
+	}
+
+}
