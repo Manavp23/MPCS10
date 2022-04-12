cc43da7702d739fad300dea29af2e189e65ebcaa
1005327001 <1005327001@L815WD220024.edu.cbe.ab.ca>
2022-04-12 15:20:24 -0600

    this is ex18

---
+++ Manav-CS10/src/Exercises/Ex18.java
@@ -0,0 +1,14 @@
+package Exercises;
+
+public class Ex18 
+{
+
+	public static void main(String[] args) 
+	{ String firstName = "John ";
+	String lastName = "Doe";
+	System.out.println(firstName.concat(lastName));
+	
+
+	}
+
+}
