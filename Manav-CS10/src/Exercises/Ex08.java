503b942beb6dfab4c808531d8d6f33e23037cd8f
1005327001 <1005327001@L815WD220024.edu.cbe.ab.ca>
2022-04-12 14:36:27 -0600

    this is ex08

---
+++ Manav-CS10/src/Exercises/Ex08.java
@@ -0,0 +1,20 @@
+package Exercises;
+
+public class Ex08 
+{
+
+	public static void main(String[] args) 
+	{ 
+		int myNum = 9;
+		float myFloatNum = 8.99f;
+		char myLetter = 'A';
+		boolean myBool = false;
+		String myText = "Hello World";
+	   System.out.println(myNum);
+	   System.out.println(myFloatNum);
+	   System.out.println(myLetter);
+	   System.out.println(myBool);
+	   System.out.println(myText);
+	}
+
+}
