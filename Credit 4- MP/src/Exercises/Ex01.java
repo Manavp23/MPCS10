2af58332692266841e5d5110a36ed84e4e324d90
1005327001 <1005327001@L815WD220024.edu.cbe.ab.ca>
2022-04-13 12:37:42 -0600

    this is ex01 of cr4

---
+++ Credit 4- MP/src/Exercises/Ex01.java
@@ -0,0 +1,13 @@
+package Exercises;
+
+public class Ex01 {
+
+	public static void main(String[] args) 
+	{ int x = 5;
+	 int y = 10;
+	Math.max(x, y);
+	System.out.println(Math.max(x, y));
+
+	}
+
+}
