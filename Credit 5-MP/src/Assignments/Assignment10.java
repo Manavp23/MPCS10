52b0d20f5e6c1839b8dbadee7f2e13d1461318e6
1005327001 <1005327001@L815WD220024.edu.cbe.ab.ca>
2022-04-26 14:12:46 -0600

    assignment 10

---
+++ Credit 5-MP/src/Assignments/Assignment10.java
@@ -0,0 +1,35 @@
+
+package Assignments;
+
+import java.util.Scanner;
+
+public class Assignment10 
+{
+
+	public static void main(String[] args) 
+	{  Scanner userinput = new Scanner(System.in);
+    System.out.println("Enter an integer: ");
+    int digit = userinput.nextInt();
+    if (digit % 2 == 0) {
+    System.out.println("The integer " +digit+ " is even");
+                } else {
+        System.out.println("The integer " +digit+ " is odd");
+		
+                }
+	}
+
+}
+/* Screen Dump
+
+Enter an integer: 
+28
+The integer 28 is even.
+
+Enter an integer:
+2049
+The integer 2049 is odd.
+
+Enter an integer:
+-236
+The integer -236 is even.
+ */
